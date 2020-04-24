f1ce6c47b8a1ad0fa6d442a92b583e1f10ddcdc1
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2020-04-24 12:40:14 -0400

    Clean up code organization

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,31 +1,28 @@
 const selectableHandsContainer = document.querySelector('.selectable-hands');
 const selectableHands = selectableHandsContainer.querySelectorAll('.hand');
 selectableHands.forEach((hand) => hand.addEventListener('click', onHandClicked));
-
 const handNames = Array.from(selectableHands).map((hand) => hand.getAttribute('data-hand'));
 
 const playedHandsContainer = document.querySelector('.played-hands');
-const playerHandElement = document.querySelector('#player-hand .hand');
-const houseHandElement = document.querySelector('#house-hand .hand');
+const playerHandElement = document.querySelector('#player-hand');
+const houseHandElement = document.querySelector('#house-hand');
 
-const scoreElement = document.getElementById('score');
 const resultElement = document.querySelector('.result');
 const resultText = document.getElementById('result-text');
 const playAgainButton = document.getElementById('play-again');
 playAgainButton.addEventListener('click', startNewRound);
 
-const rulesModal = document.querySelector('.rules .modal-wrapper');
-const closeModalButton = document.querySelector('#close-modal-button');
-const rulesButton = document.getElementById('rules-button');
-
 function startNewRound() {
   toggleVisibilityOf(selectableHandsContainer);
   toggleVisibilityOf(playedHandsContainer);
   toggleVisibilityOf(resultElement);
+
+  // clear these for the next round
   showPlayerHand('placeholder');
   showHouseHand('placeholder');
 }
 
+// .hidden applies display: none
 function toggleVisibilityOf(element) {
   element.classList.toggle('hidden');
 }
@@ -34,16 +31,16 @@ function pickRandomHouseHand() {
   return handNames[Math.floor(Math.random() * handNames.length)];
 }
 
+const handBeatenBy = {
+  rock: 'scissors',
+  scissors: 'paper',
+  paper: 'rock',
+};
+
 function getResult(playerHand, houseHand) {
   if (playerHand === houseHand) {
     return { scoreChange: 0, feedback: 'TIE GAME' };
   }
-
-  const handBeatenBy = {
-    rock: 'scissors',
-    scissors: 'paper',
-    paper: 'rock',
-  };
 
   if (handBeatenBy[playerHand] === houseHand) {
     return { scoreChange: 1, feedback: 'YOU WIN' };
@@ -67,12 +64,13 @@ function showHouseHand(houseHand) {
   houseHandElement.className = `hand ${houseHand}`;
 }
 
+const scoreElement = document.getElementById('score');
 function changeScoreBy(value) {
   scoreElement.innerHTML = Number(scoreElement.innerHTML) + value;
 }
 
 function highlightWinningHand(winner) {
-  document.querySelector(`#${winner} .hand`).classList.add('winning-hand');
+  document.getElementById(winner).classList.add('winning-hand');
 }
 
 function evaluateRoundResult(playerHand, houseHand) {
@@ -101,6 +99,10 @@ function evaluateRoundResult(playerHand, houseHand) {
     }
   }, 1500);
 }
+
+const rulesModal = document.querySelector('.rules .modal-wrapper');
+const closeModalButton = document.querySelector('#close-modal-button');
+const rulesButton = document.getElementById('rules-button');
 
 function openRulesModal() {
   rulesModal.classList.add('opened');
