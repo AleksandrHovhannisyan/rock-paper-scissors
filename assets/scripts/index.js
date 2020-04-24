93efa2cde8af383830cdae4e9f6b3f4dcea8400f
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2020-04-24 10:47:50 -0400

    Add interactivity to modal window

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,1 +1,20 @@
 const playableHands = document.querySelectorAll('#playable-hands .hand');
+const rulesModal = document.querySelector('.rules .modal-wrapper');
+const closeModalButton = document.querySelector('#close-modal-button');
+const rulesButton = document.getElementById('rules-button');
+
+function openRulesModal() {
+  rulesModal.classList.add('opened');
+}
+
+function closeRulesModal() {
+  rulesModal.classList.remove('opened');
+}
+
+rulesButton.addEventListener('click', () => {
+  openRulesModal();
+});
+
+closeModalButton.addEventListener('click', () => {
+  closeRulesModal();
+});
